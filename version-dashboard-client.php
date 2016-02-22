798eeaff694e25b095db2aa6f823fd0366faadd2
Jaakko Alajoki <jaakko@evermade.fi>
2016-02-22 13:12:31 -0500

    Initial version.

---
+++ version-dashboard-client.php
@@ -0,0 +1,119 @@
+<?php
+/*
+Plugin Name: Version Dashboard Client
+Plugin URI: http://www.evermade.fi
+Description: Provide an API endpoint for version information dashboard.
+Author: Jaakko Alajoki @jalajoki / Evermade Oy @evermadefi
+Version: 1.0.0
+Author URI: http://www.evermade.fi
+*/
+
+if ( ! function_exists( 'get_plugins' ) ) {
+    require_once ABSPATH . 'wp-admin/includes/plugin.php';
+}
+
+/**
+ * Register API endpoint.
+ */
+add_action('rest_api_init', function () {
+    register_rest_route( 'versiondashboardclient/v1', '/get_version_information/', array(
+        'methods' => 'GET',
+        'callback' => 'vdc_api_get_version_information',
+    ) );
+} );
+
+
+
+/**
+ * API endpoint handler for version query.
+ *
+ * @param  [type] $data [description]
+ * @return [type]       [description]
+ */
+function vdc_api_get_version_information($data) {
+
+	if ($data['key'] == WP_VERSION_DASHBOARD_KEY) {
+		return vdc_get_version_information();
+	} else {
+		return array('message' => 'Invalid API key. Remember to define WP_VERSION_DASHBOARD_KEY on your wp-config.php');
+	}
+
+}
+
+/**
+ * Get list of used plugins and information if they need update or not.
+ * Also get core version.
+ *
+ * Response array format:
+ *
+ * {
+ * 		plugin_count: 12,
+ * 		outdated_plugin_count: 4,
+ * 		core_current_version: 0,
+ * 		core_new_version: 0,
+ * 		core_needs_update: 0,
+ * 		plugins:
+ *      [
+ *      	{
+ *      		name: "Plugin name",
+ *      		slug: "Plugin slug",
+ *      		needs_update: true/false,
+ *      		current_version: "Version",
+ *      		new_version: "New version"
+ *       	}
+ *      ]
+ * }
+ *
+ * Note! This plugin uses 'update_plugins' transient which
+ * is updated one in a day.
+ *
+ * @return Array Array of plugin information objects.
+ */
+//add_action( 'init', 'emvdc_get_version_information' );
+function vdc_get_version_information() {
+
+    // Get core information.
+    $coreInformation = get_site_transient('update_core');
+
+    // Get plugin update info.
+    $updatePluginsTransient = get_site_transient('update_plugins');
+
+    // Response data.
+    $pluginInformation = array(
+        'plugin_count' => count($updatePluginsTransient->response) + count($updatePluginsTransient->no_update),
+        'outdated_plugin_count' => count($updatePluginsTransient->no_update),
+        'core_current_version' => $coreInformation->version_checked,
+        'core_new_version' => count($coreInformation->updates) > 0 ? $coreInformation->updates[0]->version : $coreInformation->version_checked,
+        'core_needs_update' => count($coreInformation->updates) > 0,
+        'plugins' => [],
+    );
+
+    // First add outdated plugins.
+    foreach ($updatePluginsTransient->response as $filename => $details) {
+        $pluginData = get_plugin_data(WP_PLUGIN_DIR . '/' . $filename);
+        array_push($pluginInformation['plugins'], array(
+            'name' => $pluginData['Name'],
+            'slug' => $details->slug,
+            'needs_update' => true,
+            'current_version' => $pluginData['Version'],
+            'new_version' => $details->new_version
+        ));
+    }
+
+    // Then add plugins up to date.
+    foreach ($updatePluginsTransient->no_update as $filename => $details) {
+        $pluginData = get_plugin_data(WP_PLUGIN_DIR . '/' . $filename);
+        array_push($pluginInformation['plugins'], array(
+            'name' => $pluginData['Name'],
+            'slug' => $details->slug,
+            'needs_update' => false,
+            'current_version' => $pluginData['Version'],
+            'new_version' => $details->new_version
+        ));
+    }
+
+    //print_r($pluginInformation);exit;
+
+    return $pluginInformation;
+
+}
